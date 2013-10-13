8afbd28e57c0205c169850ec2d91744d8d817dd3
jh71283 <john@greensky.co.uk>
2013-10-13 13:11:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ function rfxtrx433(opts,app) {
 };
 
 rfxtrx433.prototype.loadDevice = function(device) {
-
+_app.log.info('Loading Device');
+_app.log.info(device);
   if (this._devices[device]) return;
   this._devices[device] = new Device(device,this);
+  _app.log.info('Registering Device');
     this.emit('register',this._devices[device]);
 };
 
